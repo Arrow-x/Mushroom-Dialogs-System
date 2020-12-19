6ce002b238e69f1d6aff968d3257b8664593acb2
Arrow-x <Houssem.kun26@gmail.com>
2020-12-19 21:32:24 +0100

    instanciate cutum editors

--- Dialog System/DialogManager/Editor/CommandsTree.gd
+++ Dialog System/DialogManager/Editor/CommandsTree.gd
@@ -3,19 +3,22 @@ extends Tree
 onready var tree = self
 onready var root = tree.create_item()
 onready var current_block_label : Label = $"../inspectorHeader/inspectorHeaderHBoxContainer/CurrentBlock"
-var current_block : block
+onready var commands_settings : Panel = $"../../CommandsSettings"
 
+
+var current_block : block
+#Set up drag and droping, multiselect...
 func _ready():
 	tree.set_hide_root(true)
 
-func _on_GraphNode_graph_node_meta(meta, title):
+func _on_GraphNode_graph_node_meta(meta, title) -> void:
 	current_block = meta
 	current_block_label.text = "current block: " + title
 	tree.clear()
 	for i in meta.commands :
 		_add_command(i)
 
-func _on_add_command (id: int, pop_up : Popup):
+func _on_add_command (id: int, pop_up : Popup) -> void:
 	var _command : Command
 	if current_block == null:
 		return
@@ -32,4 +35,17 @@ func _add_command(command : Command) -> void:
 		
 	var _item : TreeItem = tree.create_item(root)
 	_item.set_text(0, command.type)
-	_item.set_meta(String(command.get_rid()), command)
+	_item.set_meta("0", command)
+
+func _on_CommandsTree_item_activated() -> void:
+	if get_selected().get_meta("0") != null:
+		if commands_settings.get_child_count() != 0:
+			if commands_settings.get_child(0) != null :
+				commands_settings.get_child(0).free()
+				
+		match get_selected().get_meta("0").type : 
+			"say" :
+				var say_control = load("res://DialogManager/Editor/Commands/SayControl.tscn").instance()
+				commands_settings.add_child(say_control, true)
+				
+
