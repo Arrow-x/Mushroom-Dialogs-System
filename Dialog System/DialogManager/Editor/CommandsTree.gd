1c5581bcba110e2079bad0916c7acc6cd4a36152
Houssem Eddine <Houssem.kun26@gmail.com>
2021-09-21 15:16:55 +0100

    Clean up the master brach from assets and the editor

--- Dialog System/DialogManager/Editor/CommandsTree.gd
+++
@@ -1,8 +0,0 @@
-extends Tree
-
-
-func _ready():
-	pass 
-
-func _recieve_block_metadeta (data):
-	print ("recived block metadata" + data)
--- Dialog System/DialogManager/Editor/FlowChartTab.gd
+++
@@ -1,8 +0,0 @@
-extends HSplitContainer
-
-onready var flowchart = FlowChart.new()
-
-func _ready():
-	pass
-func _on_graph_node_clicked(input_event):
-	pass
