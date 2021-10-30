679057b005b43be4456c375c9fdf0126b01dfae9
Arrow-x <Houssem.kun26@gmail.com>
2021-10-30 21:18:33 +0100

    scrolling through choices

--- DialogSystem/DialogManager/Editor/Commands/ForkControl.gd
+++ DialogSystem/DialogManager/Editor/Commands/ForkControl.gd
@@ -1,3 +1,3 @@
 extends Control
-onready var choices_container : Control = $ChoicesContainer
-onready var add_choice_button : Button = $ChoicesContainer/InfoBar/AddChoiceButton
+onready var choices_container : Control = $ScrollContainer/ChoicesContainer
+onready var add_choice_button : Button = $ScrollContainer/ChoicesContainer/InfoBar/AddChoiceButton
