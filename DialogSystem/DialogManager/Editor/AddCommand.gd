ac3a1e302447930e9cc5d1601b5c6b5b71e9bfd2
Arrow-x <Houssem.kun26@gmail.com>
2022-05-29 14:34:14 +0100

    Fix the add command menu

--- DialogSystem/DialogManager/Editor/AddCommand.gd
+++ DialogSystem/DialogManager/Editor/AddCommand.gd
@@ -6,6 +6,11 @@ var pop_up: Popup
 
 func _ready():
 	pop_up = get_popup()
+	pop_up.connect("id_pressed", get_node("../../../CommandsTree"), "_on_add_command", [pop_up])
+
+
+func _on_AddCommand_about_to_show():
+	pop_up.clear()
 
 	pop_up.add_item("Say Conmmand", 0)
 	pop_up.set_item_metadata(0, say_command.new())
@@ -33,5 +38,3 @@ func _ready():
 
 	pop_up.add_item("Change UI Conmmand", 8)
 	pop_up.set_item_metadata(8, change_ui.new())
-
-	pop_up.connect("id_pressed", get_node("../../../CommandsTree"), "_on_add_command", [pop_up])
