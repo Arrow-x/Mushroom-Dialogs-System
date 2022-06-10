164d0aa270a0ab528c7c871c905168b8ec4a97bd
Arrow-x <Houssem.kun26@gmail.com>
2022-06-10 23:09:08 +0100

    Opening flowchart editor safety
    Checks if the flowchart is open and switch to it, and stop the editor
    from opening the editor from opening a flowchart when it is being
    created, and if the resource is rename it update the name

--- addons/Mushroom/DialogManager/Editor/Editor.gd
+++ addons/Mushroom/DialogManager/Editor/Editor.gd
@@ -5,10 +5,16 @@ onready var flowchart_tabs: TabContainer = $FlowChartTabs
 
 
 func open_flowchart_scene(flowchart_scene: FlowChart):
-	# TODO Check if the flowchart is already open and switch to it instead
 	# TODO have the tab show if the flowchart is modified
 	# TODO add close button
 	# TODO incease of file being deleted while it is still in memory prompt of saving using the old systme
+	if flowchart_scene.get_name() == "":
+		return
+	for tab in flowchart_tabs.get_children():
+		if tab.flowchart == flowchart_scene:
+			flowchart_tabs.set_current_tab(flowchart_tabs.get_children().find(tab))
+			tab.name = flowchart_scene.get_name()
+			return
 	var ed := editor_scn.instance()
 	ed.set_flowchart(flowchart_scene)
 	flowchart_tabs.add_child(ed)
