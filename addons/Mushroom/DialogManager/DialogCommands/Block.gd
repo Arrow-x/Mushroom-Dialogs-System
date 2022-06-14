f8bfafa6dcf7d3af137bb13d18d15e6791dc11bc
Arrow-x <Houssem.kun26@gmail.com>
2022-06-14 22:13:29 +0100

    Make the Block Script a tool and it's fields serializable

--- addons/Mushroom/DialogManager/DialogCommands/Block.gd
+++ addons/Mushroom/DialogManager/DialogCommands/Block.gd
@@ -1,10 +1,11 @@
+tool
 extends Resource
 class_name block
 
 #each block should now what flowchart it is parented to
-var name: String
+export var name: String
 export(Array, Resource) var commands: Array
 
 #these are used when a block is used as condtional block
-var _next_block: Resource
-var _next_indexer: int = 0
+export var _next_block: Resource
+export var _next_indexer: int = 0
