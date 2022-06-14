329977ec2ee8939cd7d0bd9856f60265de3e14a6
Arrow-x <Houssem.kun26@gmail.com>
2022-06-14 22:12:51 +0100

    Fork command menu preview

--- addons/Mushroom/DialogManager/DialogCommands/Fork.gd
+++ addons/Mushroom/DialogManager/DialogCommands/Fork.gd
@@ -9,3 +9,13 @@ export(Array, Resource) var choices
 
 func _init():
 	f_color = Color(randf(), randf(), randf())
+
+
+func preview():
+	if choices.empty():
+		return String("Fork")
+	else:
+		var choices_str: Array
+		for c in choices:
+			choices_str.append(c.next_block.name)
+		return String("Fork to: " + String(choices_str))
