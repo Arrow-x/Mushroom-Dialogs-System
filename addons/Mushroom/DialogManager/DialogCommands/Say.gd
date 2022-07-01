f44d664421a531fee264588454e8b9ef049cdff8
Arrow-x <Houssem.kun26@gmail.com>
2022-07-01 12:50:35 +0100

    setget to the say command that emit the signal changed

--- addons/Mushroom/DialogManager/DialogCommands/Say.gd
+++ addons/Mushroom/DialogManager/DialogCommands/Say.gd
@@ -4,13 +4,31 @@ class_name say_command
 
 var type: String = "say"
 
-export var name: String
-export(String, MULTILINE) var say: String
+export var name: String setget set_say_name, get_say_name
+export(String, MULTILINE) var say: String setget set_say_text, get_say_text
 export var portrait: Texture
 export(String, "Left", "Right", "Center") var por_pos: String = "Right"
 
 export var append_text: bool = false
 
 
-func preview():
+func preview() -> String:
 	return String("Say: " + name + ": " + say)
+
+
+func set_say_name(new_name) -> void:
+	name = new_name
+	emit_signal("changed")
+
+
+func get_say_name() -> String:
+	return name
+
+
+func set_say_text(new_say) -> void:
+	say = new_say
+	emit_signal("changed")
+
+
+func get_say_text() -> String:
+	return say
