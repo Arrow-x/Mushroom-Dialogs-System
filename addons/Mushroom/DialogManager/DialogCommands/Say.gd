a52d736e8043e642cd87f4f83f0fd1e443239919
Arrow-x <Houssem.kun26@gmail.com>
2023-03-01 18:57:27 +0100

    remove useless gets

--- addons/Mushroom/DialogManager/DialogCommands/Say.gd
+++ addons/Mushroom/DialogManager/DialogCommands/Say.gd
@@ -4,8 +4,8 @@ class_name say_command
 
 var type: String = "say"
 
-export var name: String setget set_say_name, get_say_name
-export(String, MULTILINE) var say: String setget set_say_text, get_say_text
+export var name: String setget set_say_name
+export(String, MULTILINE) var say: String setget set_say_text
 export var portrait: Texture
 export(String, "Left", "Right", "Center") var por_pos: String = "Right"
 
@@ -21,14 +21,6 @@ func set_say_name(new_name) -> void:
 	emit_signal("changed")
 
 
-func get_say_name() -> String:
-	return name
-
-
 func set_say_text(new_say) -> void:
 	say = new_say
 	emit_signal("changed")
-
-
-func get_say_text() -> String:
-	return say
