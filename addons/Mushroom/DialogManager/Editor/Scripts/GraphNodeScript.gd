73b933a96b146d71b509767b9558cf2743d41581
Arrow-x <Houssem.kun26@gmail.com>
2023-10-20 23:58:37 +0100

    making the sides more clear

--- addons/Mushroom/DialogManager/Editor/Scripts/GraphNodeScript.gd
+++ addons/Mushroom/DialogManager/Editor/Scripts/GraphNodeScript.gd
@@ -24,25 +24,23 @@ func _ready() -> void:
 
 func delete_inputs(fork: ForkCommand) -> void:
 	var block: Block = get_meta("block")
-	remove_slot(block.inputs, c_inputs, false, fork)
+	remove_slot(block.inputs, c_inputs, true, fork)
 
 
 func delete_outputs(fork: ForkCommand) -> void:
 	var block: Block = get_meta("block")
-	remove_slot(block.outputs, c_outputs, true, fork)
+	remove_slot(block.outputs, c_outputs, false, fork)
 
 
 # remove a slot from true:  right, false: left
-func remove_slot(
-	meta_slots: Array, control_slots: Array, left_or_right: bool, fork: ForkCommand
-) -> void:
+func remove_slot(meta_slots: Array, control_slots: Array, left: bool, fork: ForkCommand) -> void:
 	var idx := meta_slots.find(fork)
 
 	for f in meta_slots:
-		if left_or_right:
-			set_slot_enabled_right(meta_slots.find(f), false)
-		else:
+		if left:
 			set_slot_enabled_left(meta_slots.find(f), false)
+		else:
+			set_slot_enabled_right(meta_slots.find(f), false)
 
 	if control_slots.size() > 0:
 		control_slots[idx].queue_free()
@@ -50,14 +48,14 @@ func remove_slot(
 	meta_slots.erase(fork)
 
 	for f in meta_slots:
-		if left_or_right:
-			set_slot_enabled_right(meta_slots.find(f), true)
-			set_slot_type_right(meta_slots.find(f), 1)
-			set_slot_color_right(meta_slots.find(f), f.f_color)
-		else:
+		if left:
 			set_slot_enabled_left(meta_slots.find(f), true)
 			set_slot_type_left(meta_slots.find(f), 1)
 			set_slot_color_left(meta_slots.find(f), f.f_color)
+		else:
+			set_slot_enabled_right(meta_slots.find(f), true)
+			set_slot_type_right(meta_slots.find(f), 1)
+			set_slot_color_right(meta_slots.find(f), f.f_color)
 
 
 func add_g_node_output(fork: ForkCommand) -> void:
