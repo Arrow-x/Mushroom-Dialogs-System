ff587e8f02e6020744b3a1d17f4567de57957608
Arrow-x <Houssem.kun26@gmail.com>
2023-11-29 19:36:48 +0100

    Fix Typewriter effect not starting again

--- addons/Mushroom/DialogManager/DialogUI/TypeWriter.gd
+++ addons/Mushroom/DialogManager/DialogUI/TypeWriter.gd
@@ -2,28 +2,28 @@ extends RichTextLabel
 
 @export_range(0, 1000) var speed: int = 50
 
-@onready var tween: Tween = create_tween()
+var tween: Tween
 
 signal message_done
 signal message_start
 
 
-func _ready():
+func send_message(new_text: String, append: bool) -> void:
+	visible_ratio = 0
 	var sbb := SpeedBB.new()
 	sbb.caller = self
 	install_effect(sbb)
+	tween = create_tween()
 	tween.finished.connect(tween_done)
-
-
-func send_message(new_text: String, append: bool) -> void:
-	message_start.emit()
 	if append:
 		append_text(new_text)
 	else:
 		text = new_text
 
-	tween.set_speed_scale(speed)
-	tween.tween_property(self, "visible_ratio", 1.0, get_total_character_count() / 2)
+	message_start.emit()
+	tween.set_speed_scale(speed).tween_property(
+		self, "visible_ratio", 1.0, get_total_character_count() / 2
+	)
 
 
 func set_speed(val: float) -> void:
