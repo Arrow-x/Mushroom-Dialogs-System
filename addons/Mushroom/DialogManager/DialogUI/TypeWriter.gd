609ade9c9dbffc0babef6d990ffbafbc17e694cc
Arrow-x <Houssem.kun26@gmail.com>
2023-12-01 09:23:05 +0100

    tween constant speed when append

--- addons/Mushroom/DialogManager/DialogUI/TypeWriter.gd
+++ addons/Mushroom/DialogManager/DialogUI/TypeWriter.gd
@@ -22,7 +22,7 @@ func send_message(new_text: String, append: bool) -> void:
 
 	message_start.emit()
 	tween.set_speed_scale(speed).tween_property(
-		self, "visible_characters", get_total_character_count(), get_total_character_count() / 2
+		self, "visible_characters", get_total_character_count(), new_text.length() / 2
 	)
 
 
